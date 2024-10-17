c78ef386ff93078b81f80ae1732a9e19c80bf821
dequeirozmarcondes <dequeirozmarcondes@outlook.com>
2024-10-17 10:41:49 -0400

    full chapter 11

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,32 @@
 export default function Page() {
-  return <p>Customers</p>;
+  const name = ["Kassy", "Kassy", "Kassy", "Kassy", "Kassy", "Kassy"];
+  const fontSizes = [
+    "text-2xl",
+    "text-3xl",
+    "text-4xl",
+    "text-5xl",
+    "text-6xl",
+  ];
+  const colors = [
+    "text-red-500",
+    "text-blue-500",
+    "text-green-500",
+    "text-purple-500",
+    "text-yellow-500",
+  ];
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen space-y-4">
+      {name.map((name, index) => (
+        <p
+          key={index}
+          className={`${fontSizes[index % fontSizes.length]} ${
+            colors[index % colors.length]
+          } font-bold`}
+        >
+          {name}
+        </p>
+      ))}
+    </div>
+  );
 }

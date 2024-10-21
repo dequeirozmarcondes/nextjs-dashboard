7d0ea0f1072d224178b377d51631a71f879c2a14
dequeirozmarcondes <dequeirozmarcondes@outlook.com>
2024-10-21 15:15:31 -0400

    full chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -54,7 +54,49 @@ export type State = {
 };
 
 //Função para criar um novo invoice
-export async function createInvoice(prevState: State, formData: FormData) {
+// export async function createInvoice(state: State, formData: FormData) {
+//   // Validate form using Zod
+//   const validatedFields = CreateInvoice.safeParse({
+//     customerId: formData.get("customerId"),
+//     amount: formData.get("amount"),
+//     status: formData.get("status"),
+//   });
+
+//   // If form validation fails, return errors early. Otherwise, continue.
+//   if (!validatedFields.success) {
+//     return {
+//       ...state,
+//       errors: validatedFields.error.flatten().fieldErrors,
+//       message: "Missing Fields. Failed to Create Invoice.",
+//     };
+//   }
+
+//   // Prepare data for insertion into the database
+//   const { customerId, amount, status } = validatedFields.data;
+//   const amountInCents = amount * 100;
+//   const date = new Date().toISOString().split("T")[0];
+
+//   // Insert data into the database
+//   try {
+//     await sql`
+//       INSERT INTO invoices (customer_id, amount, status, date)
+//       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+//     `;
+//   } catch (error) {
+//     // If a database error occurs, return a more specific error.
+//     return {
+//       message: {
+//         "Database Error: Failed to Create Invoice.": error,
+//       },
+//     };
+//   }
+
+//   // Revalidate the cache for the invoices page and redirect the user.
+//   revalidatePath("/dashboard/invoices");
+//   redirect("/dashboard/invoices");
+// }
+
+export async function createInvoice(state: State, formData: FormData) {
   // Validate form using Zod
   const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get("customerId"),
@@ -62,9 +104,10 @@ export async function createInvoice(prevState: State, formData: FormData) {
     status: formData.get("status"),
   });
 
-  // If form validation fails, return errors early. Otherwise, continue.
+  // Se a validação falhar, retorne os erros.
   if (!validatedFields.success) {
     return {
+      ...state,
       errors: validatedFields.error.flatten().fieldErrors,
       message: "Missing Fields. Failed to Create Invoice.",
     };
@@ -82,17 +125,22 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
-    // If a database error occurs, return a more specific error.
     return {
-      message: {
-        "Database Error: Failed to Create Invoice.": error,
-      },
+      ...state,
+      message: "Database Error: Failed to Create Invoice.",
+      error,
     };
   }
-
   // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
+
+  // Redirect or update state accordingly
+  return {
+    ...state,
+    message: "Invoice created successfully!",
+    errors: {},
+  };
 }
 
 //     customerId: formData.get("customerId"),
